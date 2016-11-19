1b18b6406095cdb5c7e226ea96291ee05f58bcde
ABHISHEK GUHA <abhi.guha.jss97@gmail.com>
2016-11-19 19:04:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 app.use(express.static('ui'));
-app.use(express.static('js'));
-app.use(express.static('css'));
-app.use(express.static('images'));
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
